de500a0a8d88ff7fb6b596081cf4d6b63bedb52a
Lukas Holzner <lukas.holzner@tum.de>
2018-06-09 17:22:08 +0200

    #17 added __init__.py files for automatic retrieval of packages, ignore deb_dist/ directory, added desktop-file, moved entry point, added setup call for linux (not installable yet...)

--- setup.py
+++ setup.py
@@ -1,23 +1,42 @@
 import sys
-from cx_Freeze import setup, Executable
 import os
-os.environ['TCL_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
-
-build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
 
 base = None
 if sys.platform == "win32":
+    from cx_Freeze import setup, Executable
+
+    os.environ['TCL_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
+    os.environ['TK_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
+
+    build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
+    
     base = "Win32GUI"
 
-setup(name="DataGorri",         # Edited by JH
-      version="1.1",            # Edited by JH
-      options={"build_exe": build_exe_options},
-      executables=[Executable("DataGorri.py",
-                              shortcutName="DataGorri",
-                              shortcutDir="DesktopFolder",
-                              base=base,
-                              icon="favicon.ico"
-                              )
-                   ]
-      )
+    setup(name="DataGorri",         # Edited by JH
+          version="1.1",            # Edited by JH
+          options={"build_exe": build_exe_options},
+          executables=[Executable("datagorri/DataGorri.py",
+                                  shortcutName="DataGorri",
+                                  shortcutDir="DesktopFolder",
+                                  base=base,
+                                  icon="favicon.ico"
+                                  )
+                       ]
+          )
+
+if sys.platform == "linux" or sys.platform == "linux2":    
+    from setuptools import setup, find_packages
+
+    setup(
+        name="DataGorri",
+        version="1.2",
+        description="Application to extract data from tables and lists located on websites",
+        url="https://github.com/julhac/datagorri",
+        packages=find_packages(),
+        entry_points={
+            'gui_scripts': ['datagorri=datagorri.DataGorri']
+        },
+        data_files=[
+            ('share/applications/', ['datagorri.desktop'])
+        ]
+    )

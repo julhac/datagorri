5973f67c57b2864401a4b9baabd3c0f12437aafe
Lukas Holzner <lukas.holzner@tum.de>
2018-06-12 21:45:37 +0200

    #17 fixed installer creation for windows

---
+++ DataGorri.py
@@ -0,0 +1,15 @@
+"""
+The Datagorri app
+
+This program can be used to scrape data out of tables located on websites on the internet
+
+.. moduleauthor:: Julian Hackinger <Julian.Hackinger@tum.de>
+
+"""
+from datagorri import App
+
+def main(args=None):
+    app = App()
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -5,24 +5,27 @@ base = None
 if sys.platform == "win32":
     from cx_Freeze import setup, Executable
 
-    os.environ['TCL_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
-    os.environ['TK_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
+    tcl = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32"
+#    tcl = "C:\\Users\\lukas\\Anaconda3"
 
-    build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
+    os.environ['TCL_LIBRARY'] = tcl + "\\tcl\\tcl8.6" 
+    os.environ['TK_LIBRARY'] = tcl + "\\tcl\\tk8.6" 
+
+    build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy","idna"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
     
     base = "Win32GUI"
 
     setup(name="DataGorri",         # Edited by JH
-          version="1.1",            # Edited by JH
+          version="1.2",            # Edited by JH
           options={"build_exe": build_exe_options},
-          executables=[Executable("datagorri/DataGorri.py",
+          executables=[Executable("DataGorri.py",
                                   shortcutName="DataGorri",
                                   shortcutDir="DesktopFolder",
                                   base=base,
                                   icon="favicon.ico"
                                   )
                        ]
-          )
+          ) 
 
 if sys.platform == "linux" or sys.platform == "linux2":    
     from setuptools import setup, find_packages

7e3490a65280f2a7958f260ae3e2133c29caa3d1
julhac <30419477+julhac@users.noreply.github.com>
2018-06-26 09:36:24 +0200

    Update app.py

--- config/app.py
+++ config/app.py
@@ -1,5 +1,5 @@
 config = dict(
-    title="DataGorri 1.1",
+    title="DataGorri 1.2",
     full_screen=True,
     request_headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0)'},
     default_url='https://en.wikipedia.org/wiki/Iris_flower_data_set',

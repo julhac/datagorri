c18fd1099a57cf0f5dc121c837a39dd6def2080d
Lukas Holzner <lukas.holzner@tum.de>
2018-06-26 17:00:46 +0200

    fixed installer creation and version number

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import os
 os.environ['TCL_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
 os.environ['TK_LIBRARY'] = "C:\\Users\\ga47dop\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
 
-build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
+build_exe_options = {"packages": ["tkinter","bs4","requests","json","csv","os","hashlib","time","datetime","urllib.parse","sys","copy","datagorri","config","idna"],"include_msvcr": False,"bin_excludes":["VCRUNTIME140.dll"],"include_files":["tcl86t.dll","tk86t.dll","samples","docs"]}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(name="DataGorri",         # Edited by JH
-      version="1.1",            # Edited by JH
+      version="1.2",            # Edited by JH
       options={"build_exe": build_exe_options},
       executables=[Executable("DataGorri.py",
                               shortcutName="DataGorri",
